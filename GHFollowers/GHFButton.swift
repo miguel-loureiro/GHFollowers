7dc2f607934097b74dca147411b2ff3aa078a297
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2024-03-01 16:59:40 +0000

    add custom buttom and custom text field

---
+++ GHFollowers/GHFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GHFButton.swift
+//  GHFollowers
+//
+//  Created by António Loureiro on 01/03/2024.
+//
+
+import UIKit
+
+class GHFButton: UIButton {
+
+    override init(frame: CGRect) {
+
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(backgroundColor: UIColor, title: String) {
+
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+
+    private func configure() {
+
+        layer.cornerRadius = 10.0
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ GHFollowers/GHFTextField.swift
@@ -0,0 +1,42 @@
+//
+//  GHFTextField.swift
+//  GHFollowers
+//
+//  Created by António Loureiro on 01/03/2024.
+//
+
+import UIKit
+
+class GHFTextField: UITextField {
+
+    override init(frame: CGRect) {
+
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius =  10.0
+        layer.borderWidth =  2.0
+        layer.borderColor = UIColor.systemGray4.cgColor
+
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth =  true
+        minimumFontSize = 12.0
+
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+
+        placeholder = "Enter a username"
+    }
+
+}
