1f85c9a8bd8d71bf8e2467b66dd84c8f027ac96d
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2024-04-15 19:10:32 +0100

    optimization and clean up part 5; persistence manager

--- GHFollowers/Controllers/FavoritesListViewController.swift
+++ GHFollowers/Controllers/FavoritesListViewController.swift
@@ -106,14 +106,18 @@ extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegat
         guard editingStyle == .delete else { return }
 
         let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
 
         PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
 
             guard let self = self else { return }
 
-            guard let error = error else { return }
+            guard let error = error else {
+                
+                //coloca-se o self para se ter acesso ao favorites da func, claro.
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
 
             self.presentGHFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
         }
--- GHFollowers/Managers/PersistenceManager.swift
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -62,24 +62,23 @@ enum PersistenceManager {
 
             switch result {
 
-                case .success(let favorites):
-                    var retrievedFavorites = favorites
+                case .success(var favorites):
 
                     switch actionType {
 
                     case .add:
-                        guard !retrievedFavorites.contains(favorite) else {
+                        guard !favorites.contains(favorite) else {
 
                             completed(.alreadyInFavorites)
                             return
                         }
-                        retrievedFavorites.append(favorite)
+                        favorites.append(favorite)
 
                     case .remove:
-                        retrievedFavorites.removeAll {  $0.login == favorite.login }
+                        favorites.removeAll {  $0.login == favorite.login }
                     }
 
-                    completed(saveFavorites(favorites: retrievedFavorites))
+                    completed(saveFavorites(favorites: favorites))
 
                 case .failure(let error):
                     completed(error)
